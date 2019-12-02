81263fd5b8177c22692c6f69825676148a8f8f31
Eddie Bell <ejlbell@gmail.com>
2019-12-02 16:53:06 +0000

    mega fancy for fast future projects

--- 002.py
+++ 002.py
@@ -1,38 +1,51 @@
 class Computer:
     def __init__(self, program):
-        self.memory = program[:]
+        self.memory = list(program)
         self.ip = 0
+
+        self.opcodes = {
+            1: {"name": "add", "size": 4, "body": self.opcode_add},
+            2: {"name": "mul", "size": 4, "body": self.opcode_mul},
+        }
+
+    def opcode_add(self, a, b, p):
+        self.write(p, self.read(a) + self.read(b))
+
+    def opcode_mul(self, a, b, p):
+        self.write(p, self.read(a) * self.read(b))
 
     def read(self, address):
         return self.memory[address]
+
+    def read_range(self, start, stop):
+        return self.memory[start:stop]
 
     def write(self, address, value):
         self.memory[address] = value
 
     def run(self):
         while True:
-            statement = self.memory[self.ip : self.ip + 4]
-            opcode = statement[0]
+            opcode = self.read(self.ip)
 
+            # special halting opcode
             if opcode == 99:
                 break
 
-            value_1 = self.read(statement[1])
-            value_2 = self.read(statement[2])
-            position = statement[3]
+            opcode_config = self.opcodes[opcode]
 
-            if opcode == 1:
-                self.write(position, value_1 + value_2)
-            elif opcode == 2:
-                self.write(position, value_1 * value_2)
-            else:
-                raise Exception("Unknown op code")
+            if opcode not in self.opcodes.keys():
+                raise ValueError("unknown opcode {}".format(opcode))
 
-            self.ip += 4
+            args = self.read_range(self.ip + 1, self.ip + opcode_config["size"])
+
+            # execute opcode with arguments
+            opcode_config["body"](*args)
+
+            self.ip += opcode_config["size"]
 
 
 if __name__ == "__main__":
-    program_rom = list(map(int, open("002.txt").read().split(",")))
+    program_rom = tuple(map(int, open("002.txt").read().split(",")))
 
     for noun in range(0, 100):
         for verb in range(0, 100):
