8661b59abe9205e4b55670e82d93c09c1ed32a25
Eddie Bell <ejlbell@gmail.com>
2019-12-06 15:03:20 +0000

    tiny

--- 006.py
+++ 006.py
@@ -1,20 +1,15 @@
-import itertools
 import networkx as nx
 
 orbits = open('006.txt', 'r').readlines()
 edges = [o.strip().split(')') for o in orbits]
-nodes = set(itertools.chain(*edges))
 
 G = nx.DiGraph()
-G.add_nodes_from(nodes)
 G.add_edges_from(edges)
 
-ancestors = sum([len(nx.ancestors(G, n)) for n in nodes])
+ancestors = sum([len(nx.ancestors(G, n)) for n in G.nodes()])
 print('direct + indirect', ancestors)
 
-G = nx.Graph()
-G.add_nodes_from(nodes)
-G.add_edges_from(edges)
+G = G.to_undirected()
 
 shortest_path = nx.astar_path_length(G, source='YOU', target='SAN')-2
 print('path', shortest_path)
